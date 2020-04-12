f861e67e5582f384f8fe2779f7f5e878e8de3ef7
Sam Bentley <samtbentley@gmail.com>
2020-04-12 19:33:17 +0100

    enabled cors

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const config = require('./config');
 const express = require('express');
-const app = express();
+var cors = require('cors')
+var app = express()
+app.use(cors())
 const port = 3000;
 const fetch = require('node-fetch');
+const logger = require('pino')()
+
 global.Headers = fetch.Headers;
 
+// Make sure to add clientId field to config.js and do not include in vcs
 const clientId = config.clientId;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/hello', (req, res) => res.send('Hello World!'));
 
@@ -14,7 +25,8 @@ app.get('/users', function (req, res) {
     getUsers(res);
 });
 
-app.get('/streams', function (req, res) {
+app.get('/streams', function (req, res, next) {
+    logger.debug("request on streams")
     res.setHeader('Content-Type', 'application/json');
     getStreams(res);
 });
@@ -34,7 +46,7 @@ function getUsers(res) {
 
     fetch("https://api.twitch.tv/helix/users?login=Makaira&login=snoewwolf&login=BoothTl&login=Jhonsk&login=RandomWelshman", requestOptions)
     .then(response => response.text())
-    .then(result => res.send(result))
+    .then(result => sendResponse(res, result))
     .catch(error => console.log('error', error));
 }
 
@@ -50,6 +62,12 @@ function getStreams(res) {
 
     fetch("https://api.twitch.tv/helix/streams?user_login=Makaira&user_login=snoewwolf&user_login=BoothTl&user_login=Jhonsk&user_login=RandomWelshman", requestOptions)
     .then(response => response.text())
-    .then(result => res.send(result))
+    .then(result => sendResponse(res, result))
     .catch(error => console.log('error', error));
+}
+
+function sendResponse(res, result) {
+    console.log('test');
+    logger.info(result.json);
+    res.send(result);
 }
